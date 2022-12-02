a1907a2e6616b10be94adaff96d01b1d8037a82a
Lukblan <luk.blanko@gmail.com>
2022-12-01 21:28:31 -0300

    Calculator now implements basic mathematical operations

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-let currentOperation = "0";
+let currentOperation = "";
+let currentNumber = "0"
 
-addEventToNumberButtons()
+addEventToNumberButtons();
+addEventToClearButton();
+addEventToDeleteButton();
+addEventToOperators();
+addEventToEqualButton();
 
 function displayCurrentOperation() {
-  const calculatorDisplay = document.getElementById("currentNumber");
+  const calculatorDisplay = document.getElementById("currentOperation");
   calculatorDisplay.innerText = currentOperation;
+  currentNumber = "0"
+  displayCurrentNumber()
+}
+
+function displayCurrentNumber() {
+  const calculatorDisplay = document.getElementById("currentNumber");
+  calculatorDisplay.innerText = currentNumber;
 }
 
 function evaluate(expression) {
@@ -17,6 +29,21 @@ function evaluate(expression) {
     leftFactor = arrayExpression[0];
     rightFactor = arrayExpression[1]
     result = Number(leftFactor) + Number(rightFactor);
+  } else if (expression.includes("-")) {
+    arrayExpression = expression.split("-");
+    leftFactor = arrayExpression[0];
+    rightFactor = arrayExpression[1]
+    result = Number(leftFactor) - Number(rightFactor);
+  } else if (expression.includes("x")) {
+    arrayExpression = expression.split("x");
+    leftFactor = arrayExpression[0];
+    rightFactor = arrayExpression[1]
+    result = Number(leftFactor) * Number(rightFactor);
+  } else if (expression.includes("/")) {
+    arrayExpression = expression.split("/");
+    leftFactor = arrayExpression[0];
+    rightFactor = arrayExpression[1]
+    result = Number(leftFactor) / Number(rightFactor);
   }
   return result;
 }
@@ -28,11 +55,72 @@ function addEventToNumberButtons() {
 
 function pressButton(event) {
   let numberPressed = event.target.firstChild.textContent;
-  if(currentOperation.length === 1 && currentOperation === "0") {
-    currentOperation = numberPressed;
+  if(currentNumber.length === 1 && currentNumber === "0") {
+    currentNumber = numberPressed;
   } else {
-    currentOperation += numberPressed;
+    currentNumber += numberPressed;
+  }
+  displayCurrentNumber()
+}
+
+function addEventToClearButton() {
+  const clearButton = document.getElementById("clear-button");
+  clearButton.addEventListener("click", clearOperations);
+}
+
+function clearOperations() {
+  currentOperation = "0";
+  currentNumber = "0";
+  displayCurrentOperation();
+}
+
+function addEventToDeleteButton() {
+  const clearButton = document.getElementById("delete-button");
+  clearButton.addEventListener("click", deleteLastNumber);
+}
+
+function deleteLastNumber() {
+  if (currentNumber.length === 1 && currentNumber !== "0") {
+    currentNumber = "0"
   }
 
-  displayCurrentOperation()
+  if (currentNumber.length > 1) {
+    currentNumber = currentNumber.substring(0, currentNumber.length - 1);
+  }
+  displayCurrentNumber();
+}
+
+function addEventToOperators() {
+  const operation = document.getElementsByClassName("operation");
+  Array.from(operation).forEach(element => element.addEventListener("click", addOperation))
+}
+
+function addOperation(event) {
+  const operationsOptions = ["+", "-", "/", "x"];
+  let currentExpressionHaveOperator = false;
+  operationsOptions.forEach(operator => {
+    currentExpressionHaveOperator = currentExpressionHaveOperator || currentOperation.includes(operator);
+  })
+  if (!currentExpressionHaveOperator) {
+    const operator = event.target.firstChild.textContent;
+    currentOperation += currentNumber + " " +  operator + " ";
+  }
+  displayCurrentOperation();
+}
+
+function addEventToEqualButton() {
+  let equalButton = document.getElementById("equal-button");
+  equalButton.addEventListener("click", computeResult);
+}
+
+function computeResult() {
+  const calculatorDisplay = document.getElementById("currentOperation");
+  const calculatorNumber = document.getElementById("currentNumber");
+  currentOperation += currentNumber
+  let result =  evaluate(currentOperation);
+  currentOperation += " =";
+  calculatorDisplay.innerText = currentOperation;
+  calculatorNumber.innerText = result.toString();
+  currentNumber = result.toString();
+  currentOperation = "";
 }
